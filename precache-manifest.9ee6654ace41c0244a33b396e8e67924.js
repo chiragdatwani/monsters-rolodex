01ff126490a417ffd7adaa3b018079a792ec18ea
Chirag Datwani <chiragdatwani01@gmail.com>
2020-10-08 15:32:37 +0200

    Updates

---
+++ precache-manifest.9ee6654ace41c0244a33b396e8e67924.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da128e205043f7e90fbb860f26d26f4e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "19b344737e5963b23079",
+    "url": "/monsters-rolodex/static/css/main.db72ca20.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19b344737e5963b23079",
+    "url": "/monsters-rolodex/static/js/main.848de4b6.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
